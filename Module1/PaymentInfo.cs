c9bf8e7cd9e1fbb09bdbe0b5410e01a362cf97af
drtran <ktran@bemach.com>
2015-08-07 15:29:40 -0400

    Initial checkin

---
+++ Module1/PaymentInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module1
+{
+    public class PaymentInfo
+    {
+        public string paymentType { get; set; }
+        public string orderName { get; set; }
+        public string orderAddress { get; set; }
+        public string orderEmail { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Project notes - CSD 2015.
+
+I download markdown tools for VS2013 
+[here](https://visualstudiogallery.msdn.microsoft.com/0855e23e-4c4c-4c82-8b39-24ab5c5a7f79 "Markdown tools")
